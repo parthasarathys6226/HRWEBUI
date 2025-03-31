c616be5f8ca088b6993119363902b1b550f7adc9
Kiran Balan <107864725+kiranSD3@users.noreply.github.com>
2025-03-31 10:09:51 +0530

    new

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Login/login.component';
+import { register } from 'module';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+{
+    path: '', redirectTo: 'register', pathMatch:"full"
+},
+{
+    path: 'login', component: LoginComponent
+},
+{
+    path: 'register', component: RegisterComponent
+},
+{
+    path: 'dashboard', component: DashboardComponent
+}
+
+];
